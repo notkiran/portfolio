d2474fea1370054ba6ae91870065172cc64dd7c4
Kiran G <mail@kirang.me>
2022-04-15 17:08:09 +0530

    index

--- portfolio_website/src/components/Hero/Hero.js
+++ portfolio_website/src/components/Hero/Hero.js
@@ -7,16 +7,24 @@ import {
 } from "../../styles/GlobalComponents";
 import Button from "../../styles/GlobalComponents/Button";
 import { LeftSection } from "./HeroStyles";
+import { Main } from "next/document";
 
-const Hero = (props) => (
+const Hero = () => (
   <Section row nopadding>
     <LeftSection>
-      <SectionTitle mai center>
+      <SectionTitle main center>
         Welcome to
         <br />
         My Personal Portfolio
       </SectionTitle>
-      <SectionText center></SectionText>
+      <SectionText>
+        I am a full stack developer with a passion for creating beautiful and
+        user friendly websites. I have a strong background in front-end
+        development and have worked with a variety of technologies.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
     </LeftSection>
   </Section>
 );
